8a6e4a2f24cd3c14a6c6502d3c0c7d380362d2a0
hzp <hzhouping@outlook.com>
2023-02-17 15:38:38 +0800

    feat: 集成 windicss

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -2,12 +2,15 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueMacros from 'unplugin-vue-macros'
 import eslint from 'vite-plugin-eslint'
+import windiCSS from 'vite-plugin-windicss'
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   console.log(isBuild)
   const {} = viteEnv
 
   const plugins = [vue(), vueJsx(), vueMacros.vite({}), eslint()]
+
+  plugins.push(windiCSS())
 
   return plugins
 }
