247b3e86d93fce78caea347355c1643965330c4b
hzp <hzhouping@outlook.com>
2023-02-17 17:21:22 +0800

    feat: 添加 app loading 加载页

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import 'virtual:windi-base.css'
-import 'virtual:windi-components.css'
-import 'virtual:windi-utilities.css'
+import { setupAssets } from '@/styles/setupAssets'
 import { createApp } from 'vue'
 import App from './App.vue'
+import AppLoad from './AppLoad.vue'
 
 async function boostrap() {
+  setupAssets()
+
+  const appLoad = createApp(AppLoad)
+
+  appLoad.mount('#app-load')
+
   const app = createApp(App)
 
   app.mount('#app')
---
+++ src/styles/setupAssets.ts
@@ -0,0 +1,7 @@
+import 'virtual:windi-base.css'
+import 'virtual:windi-components.css'
+import 'virtual:windi-utilities.css'
+import './index.less'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function setupAssets() {}
