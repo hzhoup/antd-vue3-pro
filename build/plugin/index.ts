36291e63f6390ab751efdbb7ccdb28c67033a7c1
hzp <hzhouping@outlook.com>
2023-02-17 15:11:38 +0800

    feat: 更新 ignore 文件

--- build/plugin/index.ts
+++ build/plugin/index.ts
@@ -1,12 +1,13 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueMacros from 'unplugin-vue-macros'
+import eslint from 'vite-plugin-eslint'
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   console.log(isBuild)
   const {} = viteEnv
 
-  const plugins = [vue(), vueJsx(), vueMacros.vite({})]
+  const plugins = [vue(), vueJsx(), vueMacros.vite({}), eslint()]
 
   return plugins
 }
